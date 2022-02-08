cda3106f2bc5db6c0c77c06b35ee4ee21dfeaa6d
mativaclub <96348077+mativaclub@users.noreply.github.com>
2022-02-08 15:35:12 +0200

    new

---
+++ src/ru/skypro/Main.java
@@ -0,0 +1,9 @@
+package ru.skypro;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        PersonService personService = new PersonService();
+//        Person sarah = new Person();
+    }
+}
